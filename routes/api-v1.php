41fd3a88739533756dc1b64dda8ec38f630d8e90
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-05-11 18:37:00 -0300

    end-point Category part 1

--- routes/api-v1.php
+++ routes/api-v1.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\{
+    CategoryController,
     RegisterController
 };
 use Illuminate\Http\Request;
@@ -22,3 +23,5 @@ use Illuminate\Support\Facades\Route;
 // });
 
 Route::post('register', [RegisterController::class, 'store'])->name('api.v1.register');
+
+Route::apiResource('categories', CategoryController::class)->names('api.v1.categories');
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\Web\{
+    CategoryWebController
+};
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +19,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::resource('/categories', CategoryWebController::class)->names('web.categories');
