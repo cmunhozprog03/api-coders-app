fd6f5f7157f6314532737b2bc57375b2ecefe80b
cmunhozprog03 <cmunhozprog03@hotmail.com>
2022-05-11 15:20:35 -0300

    Relão entre tabelas

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -8,4 +8,11 @@ use Illuminate\Database\Eloquent\Model;
 class Category extends Model
 {
     use HasFactory;
+
+    // one => many
+
+    public function posts()
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -11,4 +11,27 @@ class Post extends Model
 
     const BORRADOR = 1;
     const PUBLICADO = 2;
+
+    // reverse one => many
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // many => many
+    public function tags()
+    {
+        return  $this->belongsToMany(Tag::class);
+    }
+
+    // one => many morph
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
